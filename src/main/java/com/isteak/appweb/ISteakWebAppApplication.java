f17ce5699e06d2ed712feaafb5456ba9c0a606af
xdlimao <50080337+xdlimao@users.noreply.github.com>
2023-03-17 20:18:12 -0300

    Upload do Projeto.
    Primeiro commit!

---
+++ src/main/java/com/isteak/appweb/ISteakWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.isteak.appweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISteakWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISteakWebAppApplication.class, args);
+	}
+
+}
