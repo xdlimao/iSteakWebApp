57c04ec969aff85b0110b27d215334af1c8f0539
xdlimao <danieloliveiradesouza09@outlook.com>
2023-06-24 20:28:07 -0300

    CONSERTEI O ERRO DA JPA URL, UTILIZAR ISSO AQUI
    FIQUEM ATENTO A ESSA LINHA, ELA QUE FEZ O DATABASE FUNCIONAR
    
    Co-Authored-By: Guilherzmk <87135557+Guilherzmk@users.noreply.github.com>
    Co-Authored-By: Elizabeth Pereira  <105946770+lizzBT@users.noreply.github.com>

--- src/main/java/com/isteak/appweb/ISteakWebAppApplication.java
+++ src/main/java/com/isteak/appweb/ISteakWebAppApplication.java
@@ -2,8 +2,9 @@ package com.isteak.appweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ISteakWebAppApplication {
 
 	public static void main(String[] args) {
