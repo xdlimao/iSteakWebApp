0fd9569db804c66ff0fffee6352677a83c419a8c
xdlimao <danieloliveiradesouza09@outlook.com>
2023-06-24 20:46:36 -0300

    readicionei service
    da antiga branch falecida curdPeraltaMaluco

---
+++ src/main/java/com/isteak/appweb/service/AdmLoginService.java
@@ -0,0 +1,23 @@
+package com.isteak.appweb.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.isteak.appweb.model.AdmLogin;
+import com.isteak.appweb.repository.AdmLoginRepository;
+
+@Service
+public class AdmLoginService {
+
+	@Autowired
+	private AdmLoginRepository admLoginRepository;
+
+    public boolean isValidUser(String USUARIO, String SENHA) {
+        AdmLogin admLogin = admLoginRepository.findByUSUARIO(USUARIO);
+        if (admLogin != null && admLogin.getSENHA().equals(SENHA)) {
+            return true;
+            // getSENHA é da Entidade e não do JpaRepository
+        }
+        return false;
+    }
+}
