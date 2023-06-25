3e5c2c11c4c2a02cadfcc8d3d64fb8c8dd470d8f
xdlimao <danieloliveiradesouza09@outlook.com>
2023-06-24 23:41:56 -0300

    comentário da linha que faz funcionar mas sem a entity

--- src/main/java/com/isteak/appweb/ISteakWebAppApplication.java
+++ src/main/java/com/isteak/appweb/ISteakWebAppApplication.java
@@ -3,7 +3,7 @@ package com.isteak.appweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class})
 public class ISteakWebAppApplication {
 
 	public static void main(String[] args) {
