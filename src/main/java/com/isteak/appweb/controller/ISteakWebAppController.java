fe6d4ed6a0c20a1ab5a1028042eae5f65e02f0bb
xdlimao <danieloliveiradesouza09@outlook.com>
2023-06-26 22:12:10 -0300

    Adicionei Service e o Controller de AdmLogin

---
+++ src/main/java/com/isteak/appweb/controller/ISteakWebAppController.java
@@ -0,0 +1,55 @@
+package com.isteak.appweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.isteak.appweb.model.AdmLogin;
+import com.isteak.appweb.repository.AdmLoginRepository;
+
+@Controller
+@RequestMapping("")
+public class ISteakWebAppController {
+	
+		private AdmLoginRepository admLoginRepository;
+	
+		@GetMapping("/dashboard")
+		public String getDashboard() {
+	        return "dashboard";
+		}
+
+		@GetMapping("/pedidos")
+		public String getPedidos() {
+			return "pedidos";
+		}
+		@GetMapping("/histpedidos")
+		public String getHistPedidos() {
+			return "histórico_pds";
+		}
+		@GetMapping("/cadastro")
+		public String getCadastro() {
+			return "cadastro";
+		}
+	    
+		//Não sei se o controlador está funcionando, mas esse mesmo método
+		//está no controller principal, depois vejo como fazer cada controlador
+		//de cada classe independente funciona para não bagunçar o código.
+		
+		@GetMapping("/login")
+	    public String exibirFormulario(Model model) {
+	        model.addAttribute("admLogin", new AdmLogin());
+	        return "login";
+	    }
+	    @PostMapping("/formulario")
+	    public String processarFormulario(@ModelAttribute("admLogin") AdmLogin admLogin) {
+	    	AdmLogin admLoginFromDB = admLoginRepository.findByUSUARIO(admLogin.getUSUARIO());
+	        if (admLoginFromDB != null && admLoginFromDB.getSENHA().equals(admLogin.getSENHA())) {
+	            return "redirect:/dashboard";
+	        } else {
+	    	return "redirect:/burro";
+	        }
+	    }	    
+}
---
+++ src/main/java/com/isteak/appweb/service/AdmLoginService.java
@@ -0,0 +1,22 @@
+package com.isteak.appweb.service;
+
+import org.springframework.stereotype.Service;
+
+import com.isteak.appweb.model.AdmLogin;
+import com.isteak.appweb.repository.AdmLoginRepository;
+
+@Service
+public class AdmLoginService {
+	
+	private AdmLoginRepository admLoginRepository;
+
+    public boolean isValidUser(String USUARIO, String SENHA) {
+        AdmLogin admLogin = admLoginRepository.findByUSUARIO(USUARIO);
+        if (admLogin != null && admLogin.getSENHA().equals(SENHA)) {
+            return true;
+            // getSENHA é da Entidade e não do JpaRepository
+        }
+        return false;
+    }
+}
+//insert into ADMLOGIN (USUARIO, SENHA) values ('peralta', 'alimento')
