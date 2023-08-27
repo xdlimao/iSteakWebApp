6b672846f75dbfc8c97ce5ba543a3ed71b3cadf1
xdlimao <danieloliveiradesouza09@outlook.com>
2023-08-27 16:55:03 -0300

    deixando as páginas pegar no thymeleaf

--- src/main/java/com/isteak/appweb/controller/ISteakWebAppController.java
+++ src/main/java/com/isteak/appweb/controller/ISteakWebAppController.java
@@ -14,55 +14,68 @@ import com.isteak.appweb.repository.AdmLoginRepository;
 @Controller
 @RequestMapping("")
 public class ISteakWebAppController {
-	
-		@Autowired
-		private AdmLoginRepository admLoginRepository;
-	
-		@GetMapping("/dashboard")
-		public String getDashboard() {
-	        return "dashboard";
-		}
 
-		@GetMapping("/pedidos")
-		public String getPedidos() {
-			return "pedidos";
+	@Autowired
+	private AdmLoginRepository admLoginRepository;
+
+	@GetMapping("/dashboard")
+	public String getDashboard() {
+		return "dashboard";
+	}
+
+	@GetMapping("/pedidos")
+	public String getPedidos() {
+		return "pedidos";
+	}
+
+	@GetMapping("/histpedidos")
+	public String getHistPedidos() {
+		return "histórico_pds";
+	}
+
+	@GetMapping("/cadastro")
+	public String getCadastro() {
+		return "cadastro";
+	}
+
+	@GetMapping("/configuracoes")
+	public String getConfig() {
+		return "configurações";
+	}
+
+	@GetMapping("/estatistica")
+	public String getEstatisticas() {
+		return "estatistica";
+	}
+	
+	@GetMapping("/loja")
+	public String getLoja() {
+		return "loja";
+	}
+
+	// Não sei se o controlador está funcionando, mas esse mesmo método
+	// está no controller principal, depois vejo como fazer cada controlador
+	// de cada classe independente funciona para não bagunçar o código.
+
+	@GetMapping("/")
+	public String exibirFormulario(Model model) {
+		model.addAttribute("admLogin", new AdmLogin());
+		return "login";
+	}
+
+	@GetMapping("/loginerror")
+	public String exibirFormularioErro(Model model) {
+		model.addAttribute("admLogin", new AdmLogin());
+		return "loginerror";
+	}
+
+	@PostMapping("/formulario")
+	public String processarFormulario(@ModelAttribute("admLogin") AdmLogin admLogin) {
+		AdmLogin admLoginFromDB = admLoginRepository.findByUSUARIO(admLogin.getUSUARIO());
+		if (admLoginFromDB != null && admLoginFromDB.getSENHA().equals(admLogin.getSENHA())) {
+			return "redirect:/dashboard";
+		} else {
+			return "redirect:/loginerror";
 		}
-		@GetMapping("/histpedidos")
-		public String getHistPedidos() {
-			return "histórico_pds";
-		}
-		@GetMapping("/cadastro")
-		public String getCadastro() {
-			return "cadastro";
-		}
-		@GetMapping("/burro")
-		public String getBurro() {
-			return "burro";
-		}
-	    
-		//Não sei se o controlador está funcionando, mas esse mesmo método
-		//está no controller principal, depois vejo como fazer cada controlador
-		//de cada classe independente funciona para não bagunçar o código.
-		
-		@GetMapping("/login")
-	    public String exibirFormulario(Model model) {
-	        model.addAttribute("admLogin", new AdmLogin());
-	        return "login";
-	    }
-		
-		@GetMapping("/loginerror")
-	    public String exibirFormularioErro(Model model) {
-	        model.addAttribute("admLogin", new AdmLogin());
-	        return "loginerror";
-	    }
-		
-	    @PostMapping("/formulario")
-	    public String processarFormulario(@ModelAttribute("admLogin") AdmLogin admLogin) {
-	    	AdmLogin admLoginFromDB = admLoginRepository.findByUSUARIO(admLogin.getUSUARIO());
-	        if (admLoginFromDB != null && admLoginFromDB.getSENHA().equals(admLogin.getSENHA())) {
-	            return "redirect:/dashboard";
-	        } else {
-	        	return "redirect:/loginerror";
-	        }
-	    }	    
+	}
 }
